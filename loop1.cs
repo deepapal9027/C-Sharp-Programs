e4f432d07bd09b6beed1718041f226bb967b3185
Deepa Pal <101259440+deepapal9027@users.noreply.github.com>
2022-03-09 23:39:45 +0530

    Add files via upload

---
+++ loop1.cs
@@ -0,0 +1,26 @@
+// loop
+using System;
+class while_loop
+{
+	static void Main(string[]args)
+    {
+     int ctr =0;
+    do
+     {
+	   Console.WriteLine(ctr);
+	   ctr++;
+	 }while(ctr<5);
+    }   
+}
+ 
+ /* OUTPUT-
+ 0
+ 1
+ 2
+ 3
+ 4
+ */
+ 
+
+  
+  
