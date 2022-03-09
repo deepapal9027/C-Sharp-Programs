5ded7f215708669f9af76c20e2329e7e52e2ede8
Deepa Pal <101259440+deepapal9027@users.noreply.github.com>
2022-03-09 19:56:10 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,23 @@
+//Method With Parameter and Without Return Type.
+ 
+using System;
+class Method
+{
+    static void process(int first,int second)
+        {
+		 int result = first + second;
+         Console.Write("Result is : " +result);		 
+         
+        }
+	public static void Main(string[]args)
+        {
+         process(100,100); 
+		}
+}
+/*
+  OUTPUT -
+  
+  
+*/
+  
+  
