8a78499f7c7f5844068d360978b6f606caa1d164
Deepa Pal <101259440+deepapal9027@users.noreply.github.com>
2022-03-10 00:00:28 +0530

    Update Pattern.cs

--- Pattern.cs
+++ Pattern.cs
@@ -5,18 +5,18 @@ class while_loop
 	static void Main(string[]args)
     {
       int ctr=0;
-	  int ptr;
-	  while(ctr<4)
-	  {
-		ptr=0;
-	    while(ptr<4)
-         {
-			Console.Write("*");
-             ptr=ptr=ptr+1;			
-		 }
-	   Console.WriteLine();
-	   ctr=ctr+1;
-	  }
+      int ptr;
+      while(ctr<4)
+      {
+        ptr=0;
+     while(ptr<4)
+       {
+	Console.Write("*");
+         ptr=ptr=ptr+1;			
+       }
+      Console.WriteLine();
+      ctr=ctr+1;
+     }
     }   
 }
  
